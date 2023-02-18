7de34af7f16c2a06329bf46c11d962542e4be192
Shreyas897 <Shreyasshetty897@gmail.com>
2023-02-18 16:54:27 +0530

    submit

---
+++ src/main/java/com/example/Student_Library_Management_System/Models/Card.java
@@ -0,0 +1,68 @@
+package com.example.Student_Library_Management_System.Models;
+
+import com.example.Student_Library_Management_System.Enums.CardStatus;
+import jakarta.persistence.*;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.util.Date;
+
+@Entity
+@Table(name="card")
+public class Card {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+    @CreationTimestamp
+    private Date createdOn;
+    @UpdateTimestamp
+    private Date updatedOn;
+    @Enumerated(value=EnumType.STRING)
+    private CardStatus cardStatus;
+    @OneToOne
+    @JoinColumn
+    private Student student;
+
+    public Card() {
+    }
+
+    public Student getStudent() {
+        return student;
+    }
+
+    public void setStudent(Student student) {
+        this.student = student;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public void setCreatedOn(Date createdOn) {
+        this.createdOn = createdOn;
+    }
+
+    public Date getUpdatedOn() {
+        return updatedOn;
+    }
+
+    public void setUpdatedOn(Date updatedOn) {
+        this.updatedOn = updatedOn;
+    }
+
+    public CardStatus getCardStatus() {
+        return cardStatus;
+    }
+
+    public void setCardStatus(CardStatus cardStatus) {
+        this.cardStatus = cardStatus;
+    }
+}
---
+++ src/main/java/com/example/Student_Library_Management_System/Service/StudentService.java
@@ -0,0 +1,23 @@
+package com.example.Student_Library_Management_System.Service;
+
+import com.example.Student_Library_Management_System.Enums.CardStatus;
+import com.example.Student_Library_Management_System.Models.Card;
+import com.example.Student_Library_Management_System.Models.Student;
+import com.example.Student_Library_Management_System.Repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class StudentService {
+    @Autowired
+    StudentRepository studentRepository;
+    public String createStudent(Student student){
+        Card card=new Card();
+        card.setCardStatus(CardStatus.Activated);
+        card.setStudent(student);
+        student.setCard(card);
+        studentRepository.save(student);
+        return "Student Created Successfully";
+
+    }
+}
